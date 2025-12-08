f62923108cd1acdaaffc79e040e4102cb4c9e028
masekoj <132418845+masekoj@users.noreply.github.com>
2025-12-08 13:25:41 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/bhcars-5fa1fcbb/" : "/",
   server: {
     host: "::",
     port: 8080,
