1619d96ac360b4342e0edeaeeb8dc0cbfc633a6b
masekoj <132418845+masekoj@users.noreply.github.com>
2025-12-13 16:56:05 +0200

    Set BrowserRouter basename to '/bhCars'

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/bhCars">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
